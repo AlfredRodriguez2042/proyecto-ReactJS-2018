ae6e94a127ac105047582dc709091833023fa23b
Alfredo Rodriguez <alfred2042@hotmail.com>
2020-05-08 12:07:46 -0300

    Updates

---
+++ precache-manifest.3585487560fc0cb6e91d9da4e9231368.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb158cb11ba9195ed26a51402a2f7c0",
+    "url": "/proyecto/index.html"
+  },
+  {
+    "revision": "2a7cc2f704a2b117bddb",
+    "url": "/proyecto/static/css/main.580c306b.chunk.css"
+  },
+  {
+    "revision": "66eea7e61dd85a64ac16",
+    "url": "/proyecto/static/js/2.28c95f0c.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/proyecto/static/js/2.28c95f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7cc2f704a2b117bddb",
+    "url": "/proyecto/static/js/main.a7d7211a.chunk.js"
+  },
+  {
+    "revision": "1a9c5d265ddceaea7ddb",
+    "url": "/proyecto/static/js/runtime-main.b97739e7.js"
+  },
+  {
+    "revision": "df19e46f530ff481dd2d804ec281ffe9",
+    "url": "/proyecto/static/media/cheff.df19e46f.mp4"
+  },
+  {
+    "revision": "86e785e70d60907acfab882df82abcdc",
+    "url": "/proyecto/static/media/chicken.86e785e7.jpg"
+  },
+  {
+    "revision": "5abdeed0d2d29a2c1dc2fed475fc9735",
+    "url": "/proyecto/static/media/cocina.5abdeed0.jpg"
+  },
+  {
+    "revision": "87b0f8a285a70474134925d615e54f7c",
+    "url": "/proyecto/static/media/des1.87b0f8a2.jpg"
+  },
+  {
+    "revision": "32b8fd67db252d66210e949b99b500e0",
+    "url": "/proyecto/static/media/eat-3614411_640.32b8fd67.jpg"
+  },
+  {
+    "revision": "8e7f59292ea96773f6479543aea91392",
+    "url": "/proyecto/static/media/eat.8e7f5929.jpg"
+  },
+  {
+    "revision": "064f275cf9475e923dd1bb7f1a22cde0",
+    "url": "/proyecto/static/media/food1.064f275c.jpg"
+  },
+  {
+    "revision": "f902f994c67bb33c99e3aad681f371d2",
+    "url": "/proyecto/static/media/glass-3151279_1920.f902f994.jpg"
+  },
+  {
+    "revision": "596606d4c73d0ad6f3330462deead510",
+    "url": "/proyecto/static/media/grilled-chicken-1334632_1920.596606d4.jpg"
+  },
+  {
+    "revision": "9c48ae1d05b9db76c0f2beb50935b7ab",
+    "url": "/proyecto/static/media/img1.9c48ae1d.jpg"
+  },
+  {
+    "revision": "3fe3906e2854541634d85de5dc708639",
+    "url": "/proyecto/static/media/ingredientes.3fe3906e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proyecto/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d05c1bfc354b543eb6522f35c247e8af",
+    "url": "/proyecto/static/media/los-ingredientes-basicos-de-la-gastronomia-espanola-1440x810.d05c1bfc.jpg"
+  },
+  {
+    "revision": "748a40d3d709daa1b79d641e64f78fce",
+    "url": "/proyecto/static/media/register.748a40d3.jpg"
+  },
+  {
+    "revision": "b08355d8c706d66778c47d098efcb1f6",
+    "url": "/proyecto/static/media/rest1.b08355d8.jpg"
+  },
+  {
+    "revision": "14fa73faf0b728d3f88434d409204222",
+    "url": "/proyecto/static/media/rest2.14fa73fa.jpg"
+  },
+  {
+    "revision": "17065218205973e61bd163206035589b",
+    "url": "/proyecto/static/media/the-cake-427920_1920.17065218.jpg"
+  }
+]);
